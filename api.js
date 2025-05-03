863731c0886063ac2531f1c6585e936b480f22b0
Aleksandra <aleksandra.stupiec@gmail.com>
2025-05-03 16:27:31 -0600

    cors  added, api structure changes and supabase connection  const modified

--- api.js
+++ api.js
@@ -1,20 +1,30 @@
-const express = require('express')
-const dotenv = require('dotenv')
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const app = express();
+const pool = require('./config/supabase');
 dotenv.config();
 
-const app = express()
-const port = process.env.PORT || 6543
+const port = process.env.PORT || 3000;
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(cors({
+  origin: 'http://'
+}));
 
-app.get('/', (req, res) => {
-  res.send('Backend de Horno3 working! 🎉')
-})
+// app.get('/', (req, res) => {
+//   res.send('Backend de Horno3 working! 🎉');
+// })
 
 //Aqui estan todas las rutas
-app.use(require('./routes/routes.js'))
+app.use(require('./routes/routes.js'));
+
+app.use('/activities', require('./routes/activities.js'))
+// app.use('/exceptions', require('./routes/exceptions.js'))
+// app.use('/schedules', require('./routes/schedules.js'))
+// app.use('/news', require('./routes/news.js'))
+// app.use('/experiences', require('./routes/experiences.js'))
 
 app.listen(port, () => {
-  console.log(`Running on localhost:${port}`)
+  console.log(`Running on localhost:${port}`);
 })
